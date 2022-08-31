ee717bcd9ccee1c00575fbb3065770c86af7675b
JulianHardekopf <julian.hardekopf@stud.th-luebeck.de>
2022-08-31 23:58:21 +0200

    Basic Game (frontend) + DB

--- backend/Models.py
+++ backend/Models.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 #from sqlalchemy.sql import func
 #from sqlalchemy import create_engine
-
+from flask import jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 #engine = create_engine("mysql+pymysql://root:Password123!@localhost:3306/database")
@@ -24,3 +25,22 @@ class Leaderboard(db.Model) :
         return json_post
 
 db.create_all()
+
+@app.route('/leaderboard', methods=['POST','GET'])
+def highscore():
+    if request.method == 'POST':
+        args = {
+            'score': request.json.get('score')
+        }
+        highscore = Leaderboard(score=args['score'])
+        db.session.add(highscore)
+        db.session.commit()
+        return {'message': 'successfully created'},201
+    else:
+        data = db.session.query(Leaderboard).all()
+        return jsonify(({'Highscore': [m.to_json() for m in data]}))
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
