7b34a7c9212b74e9951d2c18c59aac71517c07a0
JulianHardekopf <julian.hardekopf@stud.th-luebeck.de>
2022-08-31 20:46:20 +0200

    Basic Game (frontend) + DB

--- backend/Models.py
+++ backend/Models.py
@@ -2,12 +2,11 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from sqlalchemy.sql import func
-import mysql.connector
 from sqlalchemy import create_engine
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-
+engine = create_engine("mysql+pymysql://root:Password123!@localhost:3306/database")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Password123!@localhost:3306/demo'
 
